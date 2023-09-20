7aeae6bd7606063242c0a6b59f4cbf206be3f7c9
Kyrylo <fresh.js.ftd.@gmail.com>
2023-09-20 16:19:16 +0300

    Initial commit

---
+++ test.sql
@@ -0,0 +1,12 @@
+-- @block create table
+CREATE TABLE users(
+  name VARCHAR(256)
+);
+-- @block insert users
+INSERT INTO users
+(name) VALUES
+('User1'),
+('User2'),
+('User3'),
+('User4'),
+('User5');
